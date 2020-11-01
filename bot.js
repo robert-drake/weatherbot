8fbfc024d5d874d8e438abe825a5bbfcebea58be
Rob Drake <rgdrake@tilde.life>
2020-10-31 19:16:05 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,35 @@
+const irc = require("irc-upd");
+
+const options = {
+	userName: "robweatherbot",
+	realName: "Rob's Weather Bot",
+	password: "6uldv8",
+	port: 6697,
+	secure: true,
+	selfSigned: true,
+	channels: ["#theasylum"]
+};
+
+const owner = "robdrake";
+
+let bot = new irc.Client("irc.tilde.chat", "robweatherbot", options);
+
+bot.addListener("error", (message) => {
+	console.log('error: ' + message);
+});
+
+bot.addListener("pm", function(from, message) {
+	from = String.toLowerCase(from);
+	message = String.toLowerCase(message);
+	
+	if(from === owner) {
+		if( message === "quit") {
+			console.log("bye bye");
+			bot.disconnect();
+			process.exit();
+		}
+	} else {
+		bot.say(from, "You are not my boss!");
+		console.log(from + " tried to control me!");
+	}
+});
